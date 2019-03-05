5e8251595c84f3b0f1be70bb53b265072c1e3896
unknown <janais1991@gmail.com>
2019-03-04 19:09:47 -0600

    Added HTTP Core Library code

--- simple-http-node-server.js
+++ simple-http-node-server.js
@@ -5,9 +5,23 @@
 
 
 const http = require('http');
+const fs = require('fs');
 const port = 3000;
 
-//Handles HTTP Requests:
+const requestHandler = (request, response) => {
+  response.end(`Handling a request on port ${port}`)
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log(`You have an error:  ${err}`);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
 fs.writeFile('hello-world.txt', 'Hello to this great world', 'utf8', (err) => {
   if (err) throw err;
 
